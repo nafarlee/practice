dd0aa0959d9ef587aaae1239d591a7b3402d97eb
Nicholas Farley <ohcrap524@gmail.com>
2015-01-08 01:42:56 -0500

    added solution to mother-child age difference

---
+++ ej/chapter_5/mother_child_age_delta.js
@@ -0,0 +1,14 @@
+var ancestors = JSON.parse(require('./ancestry'));
+
+var byName = {};
+ancestors.forEach(function(person) {
+	byName[person.name] = person;
+});
+
+var hasMother = function (person) { return person.mother != null; };
+var motherInData = function (person) {return byName[person.mother] != undefined; };
+var average = function (array) { function plus(a, b) {return a + b;} return array.reduce(plus) / array.length; };
+var maternityAge = function (person) {return person.born - byName[person.mother].born; };
+
+var momAgesAtBirth = ancestors.filter(hasMother).filter(motherInData).map(maternityAge);
+console.log(average(momAgesAtBirth));
