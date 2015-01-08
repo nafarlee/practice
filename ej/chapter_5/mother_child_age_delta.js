db62a5e772be74311de6716f3c3a5288126f412e
Nicholas Farley <ohcrap524@gmail.com>
2015-01-08 02:32:07 -0500

    fixed loose-equals in mother-child age solution

--- ej/chapter_5/mother_child_age_delta.js
+++ ej/chapter_5/mother_child_age_delta.js
@@ -5,8 +5,8 @@ ancestors.forEach(function(person) {
 	byName[person.name] = person;
 });
 
-var hasMother = function (person) { return person.mother != null; };
-var motherInData = function (person) {return byName[person.mother] != undefined; };
+var hasMother = function (person) { return person.mother !== null; };
+var motherInData = function (person) {return byName[person.mother] !== undefined; };
 var average = function (array) { function plus(a, b) {return a + b;} return array.reduce(plus) / array.length; };
 var maternityAge = function (person) {return person.born - byName[person.mother].born; };
 
