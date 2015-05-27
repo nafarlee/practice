af241c2ab882d3187ef0190c63cf95c9a5658ccf
Nicholas Farley <ohcrap524@gmail.com>
2015-05-27 17:53:34 -0400

    added solution to month_names ej chapter_10

---
+++ ej/chapter_10/month_names.js
@@ -0,0 +1,19 @@
+var month = function() {
+	var exports = {};
+	var names = ["January", "February", "March", "April",
+				 "May", "June", "July", "August",
+				 "September", "October", "November", "December"];
+
+	exports.name = function (number) {
+		return names[number];
+	};
+	exports.number = function (name) {
+		return names.indexOf(name);
+	};
+	return exports;
+}();
+
+console.log(month.name(2));
+// March
+console.log(month.number("November"));
+// 10
