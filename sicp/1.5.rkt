6907888242d632e7fa16e8ce4bafcd75c380aa2d
Nicholas Farley <ohcrap524@gmail.com>
2016-02-09 18:04:12 -0500

    Add solution 1.5 to SICP

---
+++ sicp/1.5.rkt
@@ -0,0 +1,18 @@
+#lang planet neil/sicp
+
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(test 0 (p))
+
+; Under normal-order evaluation, the expression will terminate successfully, as
+; the argument y ((p) in this case) will never be evaluated, as the conditional
+; would yield true.
+;
+; Under applicative-order evaluation, it will never make it to the body of the
+; test function, as the argument evaluation will repeatedly supplant the 
+; expression with another. It will infinitely attempt to evaluate (p).
