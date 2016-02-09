4535b8203d2aef2c4f3328888c9b5f91734c6459
Nicholas Farley <ohcrap524@gmail.com>
2016-02-09 18:22:33 -0500

    Add solution 1.6 for SICP

---
+++ sicp/1.6.rkt
@@ -0,0 +1,19 @@
+#lang planet neil/sicp
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+; The problem with this new-if function is related to the order in which
+; a forms will be evaluated. Regardless of the value of the predicate, both the
+; then-clause, as well as the else-clause will be evaluated until they are a 
+; primitive value. 
+;
+; In this case, it will recursive infinitely, never even evaluating the 
+; predicate.
