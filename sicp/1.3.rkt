e494a7b48f76a1ecd3dabf32ff0461d5e565d715
Nicholas Farley <ohcrap524@gmail.com>
2016-02-09 15:41:26 -0500

    Add solution 1.3 for SICP

---
+++ sicp/1.3.rkt
@@ -0,0 +1,13 @@
+#lang planet neil/sicp
+
+(define (square x)
+  (* x x))
+
+(define (sum-squares x y)
+  (+ (square x)
+     (square y)))
+
+(define (main x y z)
+  (cond ((= x (min x y z)) (sum-squares y z))
+        ((= y (min x y z)) (sum-squares x z))
+        ((= z (min x y z)) (sum-squares x y))))
