b193c980c967112678056e7d1ca60f1fd97d51b6
Nicholas Farley <ohcrap524@gmail.com>
2016-02-09 19:15:46 -0500

    Add solution 1.8 for SICP

---
+++ sicp/1.8.rkt
@@ -0,0 +1,24 @@
+#lang planet neil/sicp
+
+(define (square x) (* x x))
+
+(define (low-enough-delta? prev-guess guess)
+  (< (abs (- guess prev-guess))
+     (* guess 0.000001)))
+
+(define (improve y x)
+  (/ (+ (/ x
+           (square y))
+        (* 2 y))
+     3))
+
+(define (cube-root-approx x) (cube-root-recur x 1 x))
+
+(define (cube-root-recur prev-guess guess x)
+  (if (low-enough-delta? prev-guess guess)
+      guess
+      (cube-root-recur guess
+                       (improve guess x)
+                       x)))
+
+(cube-root-approx 345345.0)
