6089d90a51b19171d9567f65c2224be282dbb6a2
Nicholas Farley <nicholasafarley@gmail.com>
2018-04-09 19:29:05 -0400

    Add CBT 1-1 solution

---
+++ cbt/src/cbt/1_1.clj
@@ -0,0 +1,8 @@
+(ns cbt.1-1)
+
+(defn main []
+  (println (str "Yare " "yare" ", " "daze"))
+  (println (vector "J" "o" "J" "o"))
+  (println (list "D" "I" "O"))
+  (println (hash-map :a "z" :b "a"))
+  (println (hash-set 1 1 1 1 1 2 2 3 3 4)))
--- cbt/src/cbt/core.clj
+++ cbt/src/cbt/core.clj
@@ -1,6 +1,7 @@
 (ns cbt.core
+  (require [cbt.1-1 :refer [main]])
   (:gen-class))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (main))
