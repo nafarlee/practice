d96cc4c6dfe02c27ce27330a16b1e745d60f73a2
Nicholas Farley <nicholasafarley@gmail.com>
2018-05-03 17:28:04 -0400

    Add solution to CBT 10-2

---
+++ cbt/src/cbt/10_2.clj
@@ -0,0 +1,30 @@
+(ns cbt.10-2)
+
+(def url "https://www.braveclojure.com/random-quote")
+
+(def get-quote
+  []
+  (future (slurp url)))
+
+(defn get-quotes
+  [n]
+  (-> n
+      (repeatedly get-quote)
+      doall))
+
+(def to-words (partial re-seq #"\w+"))
+
+(defn word-frequencies
+  [s]
+  (->> s
+       to-words
+       frequencies))
+
+(defn quote-word-count
+  [n]
+  (let [counts (atom {})
+        futures (get-quotes n)]
+    (doseq [q futures]
+      (swap! counts
+             #(merge-with + % (word-frequencies @q))))
+    @counts))
--- cbt/src/cbt/core.clj
+++ cbt/src/cbt/core.clj
@@ -23,6 +23,7 @@
   (:require [cbt.9-2])
   (:require [cbt.9-3])
   (:require [cbt.10-1])
+  (:require [cbt.10-2])
   (:gen-class))
 
 (defn -main
