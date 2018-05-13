c495252862a99b5c00a05f3cc66cfd15f8f77e42
Nicholas Farley <nicholasafarley@gmail.com>
2018-05-13 18:30:41 -0400

    Add solution to SICP 1.16

---
+++ sicp/1.16.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+(define (even? n)
+  (= 0 (remainder n 2)))
+
+(define (expt-iter b n)
+  (expt-iter-rec b n 1))
+
+(define (expt-iter-rec b n a)
+  (cond ((= n 0) a)
+        ((even? n) (expt-iter-rec (* b b) (/ n 2) a))
+        (else (expt-iter-rec b (- n 1) (* b a)))))
