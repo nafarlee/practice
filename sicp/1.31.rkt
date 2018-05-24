b9f58f34ba8afca3aaf728d8828b552510b6cf98
Nicholas Farley <nicholasafarley@gmail.com>
2018-05-23 23:55:55 -0400

    Finish solution to SICP 1.31

--- sicp/1.31.rkt
+++ sicp/1.31.rkt
@@ -1,5 +1,11 @@
 #lang sicp
 
+(define (product-recur term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product-recur term (next a) next b))))
+
 (define (product term a next b)
   (define (iter a result)
     (if (> a b)
@@ -9,3 +15,11 @@
 
 (define (factorial n)
   (product identity 1 inc n))
+
+(define (pi-term n)
+  (if (even? n)
+    (/ (+ n 2) (+ n 1))
+    (/ (+ n 1) (+ n 2))))
+
+(define (pi-approx n)
+  (* 4 (product pi-term 1 inc n)))
